aaf6a7f44180cd4482c1b8cee4182d75ba51c10a
Christian Maher <maher.cs@gmail.com>
2022-05-24 21:53:21 -0400

    Initial commit

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((fennel-mode .
+   ((eval . (setq-local inferior-lisp-program
+                        (concat "love " (locate-dominating-file default-directory dir-locals-file)))))))
